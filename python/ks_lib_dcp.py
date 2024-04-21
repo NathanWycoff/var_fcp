ff0f289468e6b51de85eae9c02c02d059f4e26c0
Nathan Wycoff <nathanbrwycoff@gmail.com>
2024-04-20 21:34:03 -0400

    for_i implementation; std err is bad tho

--- python/ks_lib_dcp.py
+++ python/ks_lib_dcp.py
@@ -10,11 +10,16 @@ import matplotlib.pyplot as plt
 from tensorflow_probability.substrates import jax as tfp
 tfd = tfp.distributions
 from tqdm import tqdm
+import matplotlib
+matplotlib.use('Agg')
+import scipy.stats as ss
 
 np.random.seed(123)
 
 N = 100
 P = 3
+#N = 10000
+#P = 1000
 
 def lam_h(lam, eta, tau):
     return jnp.sum(tau/2*jnp.exp(-lam*jnp.abs(eta)) - jnp.log(lam))
@@ -28,14 +33,14 @@ lam_hp = jax.grad(lam_h)
 
 ## Jax implementation
 def body_fun(val):
-    lam, eta, tau, s, diff, thresh, it, max_iters = val
+    eta, lam, tau, s, diff, thresh, it, max_iters = val
     hplam = lam_hp(lam, eta, tau)
     new_lam = jnp.power(s/(-hplam), 1./3)
     diff = jnp.max(jnp.abs(new_lam-lam))
-    return new_lam, eta, tau, s, diff, thresh, it+1, max_iters
+    return eta, new_lam, tau, s, diff, thresh, it+1, max_iters
 
 def cond_fun(val):
-    lam, eta, tau, s, diff, thresh, it, max_iters = val
+    eta, lam, tau, s, diff, thresh, it, max_iters = val
     return jnp.logical_and(diff > thresh, it<max_iters)
 
 def update_lam(eta, lam, tau, s, thresh = 1e-6, max_iters = 100):
@@ -43,8 +48,8 @@ def update_lam(eta, lam, tau, s, thresh = 1e-6, max_iters = 100):
     thresh = 1e-6
 
     diff = np.inf
-    val = (lam, eta, tau, s, diff, thresh, 0, max_iters)
-    lam, eta, tau, s, diff, thresh, it, max_iters = jax.lax.while_loop(cond_fun, body_fun, val)
+    val = (eta, lam, tau, s, diff, thresh, 0, max_iters)
+    eta, lam, tau, s, diff, thresh, it, max_iters = jax.lax.while_loop(cond_fun, body_fun, val)
     return lam
 
 X = np.random.normal(size=[N,P])
@@ -53,25 +58,55 @@ y = X[:,0] + np.random.normal(scale=sigma2,size=N)
 beta_true = np.repeat(0,P)
 beta_true[0] = 1.
 
+def body_fun_eta(p, val):
+    eta, lam, tau, s, preds = val
+    pred_other = preds - eta[p] * X[:,p]
+    #pred_other = jnp.delete(X, p, axis=1) @ jnp.delete(eta, p)
+    resid_other = y - pred_other
+    xdn2 = jnp.sum(jnp.square(X[:,p]))
+    ols = jnp.sum(X[:,p] * resid_other) / xdn2
+    #s = sigma2 / xdn2
+    thresh = (lam[p]*s[p]*tau)/2
+
+    true_pred = lambda: 0.
+    false_pred = lambda: ols + jnp.sign(ols) * (1/lam[p] * lambertw(-(s[p]*tau*jnp.square(lam[p]))/(2) * jnp.exp(-jnp.abs(ols)*lam[p])))
+    eta_new = jax.lax.cond(jnp.abs(ols) < thresh, true_pred, false_pred)
+    eta = eta.at[p].set(eta_new)
+
+    preds = pred_other + eta[p] * X[:,p]
+
+    return eta, lam, tau, s, preds
 
 def update_eta(eta, lam, X, y, sigma2, tau):
     N,P = X.shape #TOOD: self reference.
 
-    for p in range(P):
-        pred_other = jnp.delete(X, p, axis=1) @ jnp.delete(eta, p)
-        resid_other = y - pred_other
-        xdn2 = jnp.sum(jnp.square(X[:,p]))
-        ols = jnp.sum(X[:,p] * resid_other) / xdn2
-        s = sigma2 / xdn2
-        thresh = (lam[p]*s*tau)/(2)
+    x2 = jnp.sum(jnp.square(X), axis=0)
+    s = sigma2 / x2
 
-        true_pred = lambda: 0.
-        false_pred = lambda: ols + jnp.sign(ols) * (1/lam[p] * lambertw(-(s*tau*jnp.square(lam[p]))/(2) * jnp.exp(-jnp.abs(ols)*lam[p])))
-        eta_new = jax.lax.cond(jnp.abs(ols) < thresh, true_pred, false_pred)
-        eta = eta.at[p].set(eta_new)
+    preds = X @ eta
+    val = (eta, lam, tau, s, preds)
+    eta, lam, tau, s, preds  = jax.lax.fori_loop(0, P, body_fun_eta, val)
 
     return eta
 
+#def update_eta(eta, lam, X, y, sigma2, tau):
+#    N,P = X.shape #TOOD: self reference.
+#
+#    # TODO: Use jax for loop?
+#    for p in range(P):
+#        pred_other = jnp.delete(X, p, axis=1) @ jnp.delete(eta, p)
+#        resid_other = y - pred_other
+#        xdn2 = jnp.sum(jnp.square(X[:,p]))
+#        ols = jnp.sum(X[:,p] * resid_other) / xdn2
+#        s = sigma2 / xdn2
+#        thresh = (lam[p]*s*tau)/(2)
+#
+#        true_pred = lambda: 0.
+#        false_pred = lambda: ols + jnp.sign(ols) * (1/lam[p] * lambertw(-(s*tau*jnp.square(lam[p]))/(2) * jnp.exp(-jnp.abs(ols)*lam[p])))
+#        eta_new = jax.lax.cond(jnp.abs(ols) < thresh, true_pred, false_pred)
+#        eta = eta.at[p].set(eta_new)
+#
+#    return eta
 
 block_iters = 100
 block_thresh = 1e-6
@@ -82,6 +117,8 @@ lam_jit = jax.jit(update_lam)
 x2 = jnp.sum(jnp.square(X), axis=0)
 s = sigma2 / x2
 
+max_nnz = 40
+
 tau_max = 1e8
 tau_min = 1e-4
 T = 100
@@ -89,10 +126,14 @@ tau_range = np.flip(np.logspace(np.log10(tau_min), np.log10(tau_max), num = T))
 
 ## Init params
 eta = jnp.zeros(P)
-lam = 1/np.sqrt(np.diag(sigma2 * np.linalg.inv(X.T @ X))/2)
+#lam = 1/np.sqrt(np.diag(sigma2 * np.linalg.inv(X.T @ X))/2)
+lam = 1/np.sqrt(sigma2 / (2*x2))
 
-etas = np.zeros([T, P])
-lams = np.zeros([T, P])
+etas = np.zeros([T, P])*np.nan
+lams = np.zeros([T, P])*np.nan
+
+X = jnp.array(X)
+y = jnp.array(y)
 
 for t, tau in enumerate(tqdm(tau_range)):
     it = 0
@@ -109,8 +150,25 @@ for t, tau in enumerate(tqdm(tau_range)):
     etas[t,:] = eta
     lams[t,:] = lam
 
+    nnz = np.sum(eta!=0)
+    if nnz >= max_nnz:
+        break
+
+
+K_plot = np.min([5,P])
+
+ntnz = np.sum(etas!=0, axis = 0)
+top_vars = np.argpartition(ntnz, -K_plot)[-K_plot:]
+
+cols = [matplotlib.colormaps['tab20'](i) for i in range(K_plot)]
+
 fig = plt.figure()
-plt.plot(tau_range, etas)
+for vi,v in enumerate(top_vars):
+    plt.plot(tau_range, etas[:,v], color = cols[vi])
+    plt.plot(tau_range, etas[:,v] + 2.3*1/lams[:,v], color = cols[vi], linestyle='--', alpha = 0.5)
+    plt.plot(tau_range, etas[:,v] - 2.3*1/lams[:,v], color = cols[vi], linestyle='--', alpha = 0.5)
+plt.ylim(-1,2)
 plt.xscale('log')
 plt.savefig("traj.pdf")
 plt.close()
+
